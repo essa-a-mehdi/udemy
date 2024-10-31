67be485cf0890350e799a3dec6ea8b500eb6c530
Essa Mehdi <essamehdi@Essas-MacBook-Pro.local>
2024-10-31 14:14:34 +1100

    fixed error by passing in props to app.js and delcaring items array

--- custom-navbar/src/App.js
+++ custom-navbar/src/App.js
@@ -5,13 +5,34 @@ import Route from './components/Route';
 
 function App() {
 
+  const items = [
+    {
+      id: 'test1',
+      label: 'What is your favourite movie',
+      content:
+        'Good question.',
+    },
+    {
+      id: 'test2',
+      label: 'Can you build a snowman?',
+      content:
+        'Yes you can.',
+    },
+    {
+      id: 'test3',
+      label: 'Can I use CSS on a react project?',
+      content:
+        'On anything you want CSSSS 1 1 % SSS.',
+    },
+  ];
+
   return (
     <div>
       <Link to="/accordion">AccordionPage</Link>
       <Link to="/dropdown">DropdownPage</Link>
       <div>
         <Route path="/accordion">
-        <AccordionPage />
+        <AccordionPage items={items}/>
         </Route>
         <Route path="/dropdown">
         <DropdownPage />
---
+++ custom-navbar/src/components/Accordion.js
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import { GoChevronDown, GoChevronLeft } from 'react-icons/go';
+
+export default function Accordion({ items }) {
+  const [expandedIndex, setExpandedIndex] = useState(-1);
+
+  const handleClick = (nextIndex) => {
+    setExpandedIndex(prevIndex => (prevIndex === nextIndex ? -1 : nextIndex));
+  };
+
+  return (
+    <div>
+      {items.map((item, index) => {
+        const isExpanded = index === expandedIndex;
+        const icon = isExpanded ? <GoChevronDown /> : <GoChevronLeft />;
+
+        return (
+          <div>
+            <div className='headerLabel'>
+              <span onClick={() => handleClick(index)}>
+                {item.label}
+                {icon}
+              </span>
+            </div>
+            {isExpanded && <div>{item.content}</div>}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
