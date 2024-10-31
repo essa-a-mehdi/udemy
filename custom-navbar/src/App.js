b8e9f3a999b1662dc550e8b9d6ab65165822fa8e
Essa Mehdi <essamehdi@Essas-MacBook-Pro.local>
2024-10-31 15:25:59 +1100

    fixed routing for dropdown

--- custom-navbar/src/App.js
+++ custom-navbar/src/App.js
@@ -34,7 +34,7 @@ function App() {
         <Route path="/accordion">
           <AccordionPage items={items}/>
         </Route>
-        <Route path="/">
+        <Route path="/dropdown">
           <DropdownPage />
         </Route>
       </div>
