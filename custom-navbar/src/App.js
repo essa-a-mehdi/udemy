a17acd0e020fe93c18da65438390aae8552de65f
Essa Mehdi <essamehdi@Essas-MacBook-Pro.local>
2024-10-31 14:10:23 +1100

    added page and updated import for accordion link

--- custom-navbar/src/App.js
+++ custom-navbar/src/App.js
@@ -1,21 +1,9 @@
-import { useState } from 'react';
 import Link from './components/Link';
-import AccordionPage from './components/AccordionPage'
+import AccordionPage from './pages/AccordionPage'
 import DropdownPage from './pages/DropdownPage';
 import Route from './components/Route';
 
 function App() {
-  const [selection, setSelection] = useState(null);
-
-  const handleSelect = (option) => {
-    setSelection(option);
-  };
-
-  const options = [
-    { label: 'Red', value: 'red' },
-    { label: 'Green', value: 'green' },
-    { label: 'Blue', value: 'blue' },
-  ];
 
   return (
     <div>
