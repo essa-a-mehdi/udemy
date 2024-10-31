caf1ba47e7db547b0f072d493b06e9df9bca1033
Essa Mehdi <essamehdi@Essas-MacBook-Pro.local>
2024-10-31 14:57:14 +1100

    update navigation

--- custom-navbar/src/context/navigation.js
+++ custom-navbar/src/context/navigation.js
@@ -9,25 +9,27 @@ function NavigationProivder({children}) {
     const [currentPath, setCurrentPath] = useState(window.location.pathname);
 
 
-    // Handle back and forth buttons with popstate which is generated from pushstate function
+    // Handle back and forth buttons with popstate which is generated from pushstate function when left and right arrow clicked
+    // aka this will change the state of the path, so we can do a re-render
     useEffect(() => {
         const handler = () => {
             setCurrentPath(window.location.pathname);
         };
         window.addEventListener('popstate', handler);
-
+        //cleanup
         return () => {
             window.removeEventListener('popstate', handler);
         };
     }, [])
 
-    // allow navigation with pushtstate and then update the path to cuase re-render
+    // allow navigation with pushstate and then update the path to cuase re-render
+    // this function will update the url in the address bar. then we set the state of the path to this as well.
     const navigate = (to) => {
         // to is the path we will navigate to like '/dropdown'
         window.history.pushState({}, '', to);
         setCurrentPath(to);
-
     }
+
     // given navigation context to all files into app
     return <NavigationContext.Provider value={{ currentPath, navigate }}>
         {children}
