1806907424d9762dab0b9b1e1ea928533e36c506
Essa Mehdi <essamehdi@Essas-MacBook-Pro.local>
2024-10-31 15:40:03 +1100

    completed modal component

--- custom-navbar/src/pages/ModalPage.js
+++ custom-navbar/src/pages/ModalPage.js
@@ -1,9 +1,16 @@
+import { useState } from "react";
 import Modal from "../components/Modal";
 
 export default function ModalPage() {
+    const [showModal, setShowModal] = useState(false);
+    const handleClick = () => {
+        setShowModal(true);
+    };
+    
     return (
         <div>
-            <Modal />
+            <button onClick={handleClick}>Open modal text</button>
+            {showModal && <Modal />}
         </div>
     )
 }   
