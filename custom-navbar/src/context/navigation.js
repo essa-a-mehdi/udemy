6d8d3e78fdfa4f47b2da7dad8bdf114fa6bf5acc
Essa Mehdi <essamehdi@Essas-MacBook-Pro.local>
2024-10-31 11:54:43 +1100

    added a handler for back and forth clicking in browser

--- custom-navbar/src/context/navigation.js
+++ custom-navbar/src/context/navigation.js
@@ -1,9 +1,26 @@
+import { useEffect, useState } from "react";
 import { createContext } from "react";
 
 const NavigationContext = createContext();
 
 function NavigationProivder({children}) {
+    const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
+
+    // Handle back and forth buttons with popstate which is generated from pushstate function
+    useEffect(() => {
+        const handler = () => {
+            setCurrentPath(window.location.pathname);
+        };
+        window.addEventListener('popstate', handler);
+
+        return () => {
+            window.removeEventListener('popstate', handler);
+        };
+    }, [])
+
     return <NavigationContext.Provider value={{}}>
+        {currentPath};
         {children}
     </NavigationContext.Provider>
 }
