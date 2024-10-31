b4bc50edc34a715037bf8a8e4fe0c2343e7ea415
Essa Mehdi <essamehdi@Essas-MacBook-Pro.local>
2024-10-31 15:02:53 +1100

    created a custom hook for navigation to make context cleaner

--- custom-navbar/src/components/Link.js
+++ custom-navbar/src/components/Link.js
@@ -1,10 +1,9 @@
-import { useContext } from 'react'
-import NavigationContext from '../context/navigation';
+import useNavigation from "../hooks/use-navigation";
 // to is the path that we r gonna navigate to when link clicked
 // children is the text we are gonna show with anchor
 
 export default function Link({ to, children }) {
-    const { navigate } = useContext(NavigationContext);
+    const { navigate } = useNavigation;
 
     const handleClick =(event) => {
         console.log(event);
