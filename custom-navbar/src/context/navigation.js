81619e55c3269f72296895e538cfdf206343061f
Essa Mehdi <essamehdi@Essas-MacBook-Pro.local>
2024-10-31 12:01:50 +1100

    adding a function to handle clicks between pages with a button

--- custom-navbar/src/context/navigation.js
+++ custom-navbar/src/context/navigation.js
@@ -4,6 +4,8 @@ import { createContext } from "react";
 const NavigationContext = createContext();
 
 function NavigationProivder({children}) {
+    // Set a state to cause a re-render whenever the forward and back button is clicked
+    // This would cause path to change hence use path to check when state is changed
     const [currentPath, setCurrentPath] = useState(window.location.pathname);
 
 
@@ -19,8 +21,15 @@ function NavigationProivder({children}) {
         };
     }, [])
 
-    return <NavigationContext.Provider value={{}}>
-        {currentPath};
+    // allow navigation with pushtstate and then update the path to cuase re-render
+    const navigate = (to) => {
+        // to is the path we will navigate to like '/dropdown'
+        window.history.pushState({}, '', to);
+        setCurrentPath(to);
+
+    }
+    // given navigation context to all files into app
+    return <NavigationContext.Provider value={{ currentPath, navigate }}>
         {children}
     </NavigationContext.Provider>
 }
