b9c2a0130d583f9b3156d13a3d9fd245d1fcea24
Essa Mehdi <essamehdi@Essas-MacBook-Pro.local>
2024-11-01 11:30:57 +1100

    created table stucture

--- custom-navbar/src/components/Table.js
+++ custom-navbar/src/components/Table.js
@@ -1,3 +1,15 @@
-export default function Table() {
-    return <div>Table</div>;
+export default function Table({ data }) {
+    return(
+        <table>
+            <thead>
+                <tr>
+                    <th>Fruit</th>
+                    <th>Color</th>
+                    <th>Score</th>
+                </tr>
+            </thead>
+            <tbody>
+            </tbody>
+        </table>
+    )
 }
--- custom-navbar/src/pages/TablePage.js
+++ custom-navbar/src/pages/TablePage.js
@@ -1,7 +1,14 @@
 import Table from "../components/Table";
 
 export default function TablePage() {
+    const data = [
+        { name: 'Orange', color: 'orange', score: 5},
+        { name: 'Apple', color: 'red', score: 3},
+        { name: 'Banana', color: 'yellow', score: 1},
+        { name: 'Lime', color: 'green', score: 4},
+
+    ]
     return <div>
-        <Table />
+        <Table data={data}/>
     </div>
 }
