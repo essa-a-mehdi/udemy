d628ef47e616d7cf7bb53cf344d8c66d24915cf8
Essa Mehdi <essamehdi@Essas-MacBook-Pro.local>
2024-11-01 12:43:27 +1100

    renamed variable. this marks the end of section 15

--- custom-navbar/src/components/Table.js
+++ custom-navbar/src/components/Table.js
@@ -3,17 +3,17 @@ export default function Table({ data, config }) {
         return <th key={column.label}>{column.label}</th>;
     });
 
-    const renderedRows = data.map((fruit) => {
+    const renderedRows = data.map((rowData) => {
         const renderedCells = [];
         const length = config.length;
 
         for (let i = 0; i < length; i++) {
             renderedCells.push(
-                <td key={config[i].label}>{config[i].render(fruit)}</td>
+                <td key={config[i].label}>{config[i].render(rowData)}</td>
             );
         }
 
-        return <tr key={fruit.name}>{renderedCells}</tr>;
+        return <tr key={rowData.name}>{renderedCells}</tr>;
     });
 
     return (
