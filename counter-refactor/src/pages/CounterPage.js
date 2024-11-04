aae2dc97fe3385149878eb16b7ade5259c7be0d5
Essa Mehdi <essamehdi@Essas-MacBook-Pro.local>
2024-11-04 13:37:47 +1100

    returning button instead of state, moved export statement

--- counter-refactor/src/pages/CounterPage.js
+++ counter-refactor/src/pages/CounterPage.js
@@ -2,10 +2,10 @@ import { useState } from 'react';
 import Button from '../components/Button';
 import Panel from '../components/Panel';
 
-function CounterPage({ initialCount }) {
+export default function CounterPage({ initialCount }) {
   const [count, setCount] = useState(initialCount);
   const [valueToAdd, setValueToAdd] = useState(0);
-  return count;
+
+    return <button onClick={increment}></button>
 }
 
-export default CounterPage;
