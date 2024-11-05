f2d07b47390ba75859f019381a9e5074a56edcff
Essa Mehdi <essamehdi@Essas-MacBook-Pro.local>
2024-11-05 13:15:58 +1100

    created inputs for credentials

--- update-test-user/src/components/Credentials.js
+++ update-test-user/src/components/Credentials.js
@@ -1,3 +1,19 @@
 export default function Credentials() {
-
+    return (
+        <div>
+            <h2>QA Credentials</h2>
+            <div>
+                <label>Access Key</label>
+                <input type="password" placeholder="input"/>
+            </div>
+            <div>
+                <label>Secret Key</label>
+                <input type="password" placeholder="input"/>
+            </div>
+            <div>
+                <label>Sesstion Token</label>
+                <input type="password" placeholder="input"/>
+            </div>
+        </div>
+    )
 }
